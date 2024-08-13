06e9f1badc7be6dd93c32dd2b7680ecfefb2de4f
Stephen T <xxlennygamerxx@gmail.com>
2024-08-13 00:20:24 -0500

    Expand LineRange features.

--- src/Reaganism.FBI/LineRange.cs
+++ src/Reaganism.FBI/LineRange.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Reaganism.FBI;
 
 /// <summary>
@@ -12,4 +16,63 @@ public readonly record struct LineRange(int Start, int Length)
     ///     The ending line.
     /// </summary>
     public int End => Start + Length;
+
+    public bool Contains(int index)
+    {
+        return Start <= index && index < End;
+    }
+
+    public bool Contains(LineRange range)
+    {
+        return range.Start >= Start && range.End <= End;
+    }
+
+    public bool Intersects(LineRange range)
+    {
+        return range.Start < End || range.End > Start;
+    }
+
+    public override string ToString()
+    {
+        return $"[{Start},{End})";
+    }
+
+    public static LineRange operator +(LineRange range, int i)
+    {
+        return new LineRange(range.Start + i, range.End + i);
+    }
+
+    public static LineRange operator -(LineRange range, int i)
+    {
+        return new LineRange(range.Start - i, range.End - i);
+    }
+
+    public IEnumerable<LineRange> Except(IEnumerable<LineRange> except, bool presorted = false)
+    {
+        if (!presorted)
+        {
+            except = except.OrderBy(x => x.Start);
+        }
+
+        var start = Start;
+        foreach (var range in except)
+        {
+            if (range.Start - start > 0)
+            {
+                yield return new LineRange(start, range.Start);
+            }
+
+            start = range.End;
+        }
+
+        if (End - start > 0)
+        {
+            yield return new LineRange(start, End);
+        }
+    }
+
+    public static implicit operator Range(LineRange range)
+    {
+        return new Range(Index.FromStart(range.Start), Index.FromStart(range.End));
+    }
 }
