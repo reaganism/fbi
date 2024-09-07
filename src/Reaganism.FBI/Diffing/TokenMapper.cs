5b83b5ffaf66385d54c30c5aeb5d4d1de7642e16
Stephen T <xxlennygamerxx@gmail.com>
2024-09-07 15:37:39 -0500

    Inline `EnumerateWords` in `WordsToIds`. Decreases allocations greatly.

--- src/Reaganism.FBI/Diffing/TokenMapper.cs
+++ src/Reaganism.FBI/Diffing/TokenMapper.cs
@@ -103,7 +103,7 @@ public sealed class TokenMapper
     [PublicAPI]
     public string WordsToIds(string line)
     {
-        var b = 0;
+        /*var b = 0;
 
         foreach (var r in EnumerateWords(line))
         {
@@ -115,6 +115,64 @@ public sealed class TokenMapper
             }
 
             buf[b++] = (char)AddWord(word);
+        }*/
+
+        var b = 0;
+
+        // Inlined from EnumerateWords (see commented declaration for reasons).
+        for (var i = 0; i < line.Length;)
+        {
+            var start = i;
+            var c     = line[i++];
+
+            // Search for different "word" types: words, numbers, whitespace,
+            // and symbols.
+            if (char.IsLetter(c))
+            {
+                // If we start with a character, begin resolving an entire word.
+                // A word must start with a letter and may contain letters or
+                // digits.
+                while (i < line.Length && char.IsLetterOrDigit(line, i))
+                {
+                    i++;
+                }
+            }
+            else if (char.IsDigit(c))
+            {
+                // If we start with a digit, begin resolving an entire number.
+                // A number must start with a digit and may contain only digits.
+                while (i < line.Length && char.IsDigit(line, i))
+                {
+                    i++;
+                }
+            }
+            else if (c is ' ' or '\t')
+            {
+                // If we start with whitespace, begin resolving all contiguous
+                // whitespace characters of that type.  To maintain
+                // compatibility with Chicken-Bones/DiffPatch diffs, we only
+                // handle spaces and tabs.
+                while (i < line.Length && line[i] == c)
+                {
+                    i++;
+                }
+            }
+
+            // Return the resolved range.  If a character is not a supported
+            // whitespace character, a letter, or a digit, it also falls through
+            // here.  This means that symbols will consist of only a single
+            // character.
+            // yield return new Range(start, i);
+            {
+                var word = line[start..i];
+
+                if (b >= buf.Length)
+                {
+                    Array.Resize(ref buf, buf.Length * 2);
+                }
+
+                buf[b++] = (char)AddWord(word);
+            }
         }
 
         return new string(buf, 0, b);
@@ -142,6 +200,10 @@ public sealed class TokenMapper
         return idToWord[id];
     }
 
+    // EnumerateWords is inlined into WordsToIds directly to avoid heap
+    // allocations and additional overhead (WordsToIds is an immensely hot code
+    // path).
+    /*
     /// <summary>
     ///     Enumerates the words in a line, yielding ranges for each word or
     ///     symbol.
@@ -195,4 +257,5 @@ public sealed class TokenMapper
             yield return new Range(start, i);
         }
     }
+    */
 }
