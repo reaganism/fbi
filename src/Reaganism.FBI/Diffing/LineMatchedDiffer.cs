014eaf95b0969e7d8ca39e7fbe2aa1a009dfcc7a
Stephen T <xxlennygamerxx@gmail.com>
2024-09-08 01:38:32 -0500

    Reuse FuzzyLineMatcher instance between matches.

--- src/Reaganism.FBI/Diffing/LineMatchedDiffer.cs
+++ src/Reaganism.FBI/Diffing/LineMatchedDiffer.cs
@@ -11,23 +11,33 @@ namespace Reaganism.FBI.Diffing;
 public class LineMatchedDiffer(TokenMapper? tokenMapper = null) : PatienceDiffer(tokenMapper)
 {
     [PublicAPI]
-    public int MaxMatchOffset { [PublicAPI] get; [PublicAPI] set; } = MatchMatrix.DEFAULT_MAX_OFFSET;
+    public int MaxMatchOffset
+    {
+        [PublicAPI] get => fuzzyLineMatcher.MaxMatchOffset;
+        [PublicAPI] set => fuzzyLineMatcher.MaxMatchOffset = value;
+    }
 
     [PublicAPI]
-    public float MinMatchScore { [PublicAPI] get; [PublicAPI] set; } = FuzzyLineMatcher.DEFAULT_MIN_MATCH_SCORE;
+    public float MinMatchScore
+    {
+        [PublicAPI] get => fuzzyLineMatcher.MinMatchScore;
+        [PublicAPI] set => fuzzyLineMatcher.MinMatchScore = value;
+    }
+
+    private readonly FuzzyLineMatcher fuzzyLineMatcher = new()
+    {
+        MaxMatchOffset = MatchMatrix.DEFAULT_MAX_OFFSET,
+        MinMatchScore  = FuzzyLineMatcher.DEFAULT_MIN_MATCH_SCORE,
+    };
 
     [PublicAPI]
     public override int[] Match(IReadOnlyCollection<string> originalLines, IReadOnlyCollection<string> modifiedLines)
     {
-        var matches = base.Match(originalLines, modifiedLines);
+        var matches        = base.Match(originalLines, modifiedLines);
         var wordModeLines1 = originalLines.Select(TokenMapper.WordsToIds).ToArray();
         var wordModeLines2 = modifiedLines.Select(TokenMapper.WordsToIds).ToArray();
 
-        new FuzzyLineMatcher
-        {
-            MinMatchScore  = MinMatchScore,
-            MaxMatchOffset = MaxMatchOffset,
-        }.MatchLinesByWords(matches, wordModeLines1, wordModeLines2);
+        fuzzyLineMatcher.MatchLinesByWords(matches, wordModeLines1, wordModeLines2);
 
         return matches;
     }
