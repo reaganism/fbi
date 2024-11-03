f74ee3e7b547d651d38e51deb6a1fd5121c48ab9
Stephen T <xxlennygamerxx@gmail.com>
2024-11-03 01:50:00 -0600

    Move Utf16String StringBuilder extensions and make them accessible.

--- src/Reaganism.FBI/Utilities/Utf16String.cs
+++ src/Reaganism.FBI/Utilities/Utf16String.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 using JetBrains.Annotations;
 
@@ -176,4 +178,50 @@ public readonly unsafe struct Utf16String : IEquatable<Utf16String>
     {
         return !(left == right);
     }
+}
+
+public static class Utf16StringExtensions
+{
+#region StringBuilder extensions
+    // TODO: Currently dependent on implementation details of StringBuilder.
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Append")]
+    private static extern void Append(
+        StringBuilder @this,
+        ref char      value,
+        int           valueCount
+    );
+
+    /// <summary>
+    ///     Appends a given <see cref="Utf16String"/> (<paramref name="value"/>)
+    ///     to a <see cref="StringBuilder"/> without unnecessary allocation of
+    ///     <see cref="string"/> objects.
+    /// </summary>
+    /// <param name="this">The <see cref="StringBuilder"/> to append to.</param>
+    /// <param name="value">The <see cref="Utf16String"/> to append.</param>
+    public static StringBuilder AppendUtf16(
+        this StringBuilder @this,
+        Utf16String        value
+    )
+    {
+        Append(@this, ref value.Ref, value.Length);
+        return @this;
+    }
+
+    /// <summary>
+    ///     Appends a given <see cref="Utf16String"/> (<paramref name="value"/>)
+    ///     to a <see cref="StringBuilder"/> without unnecessary allocation of
+    ///     <see cref="string"/> objects followed by a newline.
+    /// </summary>
+    /// <param name="this">The <see cref="StringBuilder"/> to append to.</param>
+    /// <param name="value">The <see cref="Utf16String"/> to append.</param>
+    public static StringBuilder AppendUtf16Line(
+        this StringBuilder @this,
+        Utf16String        value
+    )
+    {
+        Append(@this, ref value.Ref, value.Length);
+        return @this.AppendLine();
+    }
+#endregion
 }
